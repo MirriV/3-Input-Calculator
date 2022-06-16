82bd201e976590b595548d51a6b1ad91bbf66156
MirriV <101925718+MirriV@users.noreply.github.com>
2022-06-16 20:49:23 +0200

    Three Input Calculator

--- JS/iftask.js
+++ JS/iftask.js
@@ -1,7 +1,18 @@
-var ageInput=document.getElementById("age");
+function calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = (document.querySelector("#operator").value);
+    var calculate;
 
-var shoeSizeInput=document.getElementById("shoeSize");
+    if (op == "add") {
+        calculate = a + b;
+    } else if (op == "min") {
+        calculate = a - b;
+    } else if (op == "div") {
+        calculate = a / b;
+    } else if (op == "mul") {
+        calculate = a * b;
+    }
 
-var operatorInput=document.getElementById("operator");
-
-
+    document.querySelector("#result").innerHTML = calculate;
+}
